c14eec5b3320111501b62f456bfbfa69d734f80a
kaku <guofei1986@gmail.com>
2012-11-16 14:41:04 +0900

    makefile

--- test/test.c
+++ test/test.c
@@ -3,22 +3,22 @@
 
 #include "../src/jsrsa.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-        char tt[] = "abc";
+  unsigned char tt[] = "abc";
 
-        generate_rsa_key_to_file("./", 1024);
+  generate_rsa_key_to_file("./", 1024);
 
-        RSA *pub = read_rsa_key_from_file("./public.pem", PUBLIC_KEY);
-        RSA *pri = read_rsa_key_from_file("./private.pem", PRIVATE_KEY);
+  RSA *pub = read_rsa_key_from_file("./public.pem", PUBLIC_KEY);
+  RSA *pri = read_rsa_key_from_file("./private.pem", PRIVATE_KEY);
 
-        unsigned char *text = public_encrypt(pub, tt, strlen(tt)*sizeof(pri2[0]));
+  unsigned char *text = public_encrypt(pub, tt, strlen(tt)*sizeof(tt[0]));
 
-        FILE *outFile = fopen("out.txt", "w");
-        fwrite(text, 1, strlen(text)*sizeof(text[0]), outFile);
-        fclose(outFile);
+  FILE *outFile = fopen("out.txt", "w");
+  fwrite(text, 1, strlen(text)*sizeof(text[0]), outFile);
+  fclose(outFile);
 
-        unsigned char *text2 = private_decrypt(pri, text, strlen(text)*sizeof(text[0]));
+  char *text2 = private_decrypt(pri, text, strlen(text)*sizeof(text[0]));
 
-        return 0;
+  return 0;
 }
